77e3fd0e302f847ae2585d63abd0ecf5f86e1401
linus123 <linus_pagan@hotmail.com>
2016-03-26 07:33:40 -0500

    Completed mine sweeper untested

--- src/110102_Minesweeper/Minesweeper.py
+++ src/110102_Minesweeper/Minesweeper.py
@@ -1,5 +1,98 @@
 def count_mines(height, width, field):
-    pass
+
+    def is_on_field(x, y):
+        if x < 0:
+            return False
+
+        if y < 0:
+            return False
+
+        if x >= width:
+            return False
+
+        if y >= height:
+            return False
+
+        return True
+
+    def create_result_field():
+        y = 0
+
+        new_field = []
+
+        while y < height:
+            new_field.append(list("0" * width))
+            y += 1
+
+        return new_field
+
+    def is_mine(x, y):
+
+        if is_on_field(x, y) and field[y][x] == "*":
+            return True
+
+        return False
+
+    def count_surrounding_mines(x, y):
+        mine_count = 0
+
+        if is_mine(x - 1, y - 1):
+            mine_count += 1
+
+        if is_mine(x, y - 1):
+            mine_count += 1
+
+        if is_mine(x + 1, y - 1):
+            mine_count += 1
+
+        if is_mine(x - 1, y):
+            mine_count += 1
+
+        if is_mine(x + 1, y):
+            mine_count += 1
+
+        if is_mine(x - 1, y + 1):
+            mine_count += 1
+
+        if is_mine(x, y + 1):
+            mine_count += 1
+
+        if is_mine(x + 1, y + 1):
+            mine_count += 1
+
+        return mine_count
+
+    def convert_to_array_of_strings(result_field):
+        y = 0
+
+        new_field = []
+
+        while y < height:
+            new_field.append("".join(result_field[y]))
+            y += 1
+
+        return new_field
+
+    y = 0
+
+    result_field = create_result_field()
+
+    while y < height:
+
+        x = 0
+
+        while x < width:
+
+            if is_mine(x, y):
+                result_field[y][x] = "*"
+            else:
+                result_field[y][x] = str(count_surrounding_mines(x, y))
+
+            x += 1
+
+        y += 1
+
+    return convert_to_array_of_strings(result_field)
 
 # def main():
 #     threeNPLus1FromStdIn()
--- src/110102_Minesweeper/MinesweeperTest.py
+++ src/110102_Minesweeper/MinesweeperTest.py
@@ -1,19 +1,33 @@
 import unittest
+
 from Minesweeper import count_mines
 
+
 class MinesweeperTests(unittest.TestCase):
-    def test_should_work(self):
-        field = []
+    def test_should_return_expected_count1(self):
+        field = ["*...", "....", ".*..", "...."]
 
-        field.append("*...")
-        field.append("....")
-        field.append(".*..")
-        field.append("....")
+        field_with_counts = count_mines(4, 4, field)
 
-        count_mines(3, 5, field)
+        self.assertEquals(field_with_counts[0], "*100")
+        self.assertEquals(field_with_counts[1], "2210")
+        self.assertEquals(field_with_counts[2], "1*10")
+        self.assertEquals(field_with_counts[3], "1110")
+
+    def test_should_return_expected_count2(self):
+        field = ["**...", ".....", ".*..."]
+
+        field_with_counts = count_mines(3, 5, field)
+
+        self.assertEquals(field_with_counts[0], "**100")
+        self.assertEquals(field_with_counts[1], "33200")
+        self.assertEquals(field_with_counts[2], "1*100")
+
+
 
 def main():
     unittest.main()
+
 
 if __name__ == '__main__':
     main()
