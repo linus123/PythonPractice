57f80a9c2b6c4eaa3f241548a67137bf76a00c9e
linus123 <linus_pagan@hotmail.com>
2016-03-26 06:14:00 -0500

    Changed default template

--- 000000_Template/ProdCode.py
+++ 000000_Template/ProdCode.py
@@ -1,9 +1,13 @@
-#def threeNPlus1(x):
+# def threeNPLus1FromStdIn():
+    # for line in sys.stdin:
+    #     if (not line.strip() == ""):
+    #         splitResult = line.split()
+    #         maxResult = maxThreeNPlus1(int(splitResult[0]), int(splitResult[1]))
+    #         print(splitResult[0] + " " + splitResult[1] + " " + str(maxResult))
 
-#def main():
-#    threeNPlus1FromFile("110101.txt")
-
-#if __name__ == '__main__':
-#    main()
-
+# def main():
+#     threeNPLus1FromStdIn()
+#
+# if __name__ == '__main__':
+#     main()
 
