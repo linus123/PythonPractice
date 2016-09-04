0b84157001bcc638972adc85b565ec6f64f9c4f9
linus123 <linus_pagan@hotmail.com>
2016-09-04 06:37:37 -0500

    Refactored code

--- src/10033_Interpreter/Interpreter.py
+++ src/10033_Interpreter/Interpreter.py
@@ -2,7 +2,7 @@ class Interpreter :
 
     def __init__(self, init_ram):
         self.DEFAULT_VALUE = '000'
-        self._registers = [self.DEFAULT_VALUE] * 10
+        self._registers = [0] * 10
         self._ram = [self.DEFAULT_VALUE] * 1000
 
         counter = 0
@@ -12,13 +12,47 @@ class Interpreter :
             counter += 1
 
     def get_register_value(self, index):
-        return self._registers[index]
+        return self._convert_to_printable_string(self._registers[index])
 
     def get_ram_value(self, index):
         return self._ram[index]
 
-    def _convert_to_valid_value_or_default(self, val):
+    def execute(self):
+        program_counter = 0
 
+        current_command = self._ram[program_counter]
+
+        while current_command != '100':
+
+            value1 = int(current_command[1])
+            value2 = int(current_command[2])
+
+            if current_command[0] == '2':
+                self._set_register_to_value(value1, value2)
+            elif current_command[0] == '3':
+                self._add_value_to_register(value1, value2)
+            elif current_command[0] == '4':
+                self._multiply_value_by_register(value1, value2)
+            elif current_command[0] == '5':
+                self._copy_register_value(value1, value2)
+
+            program_counter += 1
+
+            current_command = self._ram[program_counter]
+
+    def _set_register_to_value(self, register_index, value):
+        self._registers[register_index] = value
+
+    def _add_value_to_register(self, register_index, value):
+        self._registers[register_index] += value
+
+    def _multiply_value_by_register(self, register_index, value):
+        self._registers[register_index] *= value
+
+    def _copy_register_value(self, destination, source):
+        self._registers[destination] = self._registers[source]
+
+    def _convert_to_valid_value_or_default(self, val):
         if val is None:
             return self.DEFAULT_VALUE
 
@@ -30,6 +64,9 @@ class Interpreter :
             return self.DEFAULT_VALUE
 
         if (val_as_int < 1000):
-            return '{0:03d}'.format(val_as_int)
+            return self._convert_to_printable_string(val_as_int)
 
         return self.DEFAULT_VALUE
+
+    def _convert_to_printable_string(self, val):
+        return '{0:03d}'.format(val)
--- src/10033_Interpreter/InterpreterTests.py
+++ src/10033_Interpreter/InterpreterTests.py
@@ -24,10 +24,50 @@ class InterpreterTests(unittest.TestCase):
         for index in range(1, 1000) :
             self.assertEquals('000', interpreter.get_ram_value(index))
 
-    def test_should_ingnore_invalid_ram_value(self):
+    def test_should_ignore_invalid_ram_value(self):
         interpreter = Interpreter(['', None, 'Some non Int', '1000'])
 
         self.assertEquals('000', interpreter.get_ram_value(0))
         self.assertEquals('000', interpreter.get_ram_value(1))
         self.assertEquals('000', interpreter.get_ram_value(2))
         self.assertEquals('000', interpreter.get_ram_value(3))
+
+    def test_should_do_nothing_given_single_halt_value(self):
+        interpreter = Interpreter(['100'])
+        interpreter.execute()
+
+        for index in range(10) :
+            self.assertEquals('000', interpreter.get_register_value(index))
+
+    def test_2dn_should_set_register_d_to_n(self):
+        interpreter = Interpreter(['201', '100'])
+        interpreter.execute()
+        self.assertEquals('001', interpreter.get_register_value(0))
+
+        interpreter = Interpreter(['299', '100'])
+        interpreter.execute()
+        self.assertEquals('009', interpreter.get_register_value(9))
+
+    def test_3dn_should_add_n_to_register_d(self):
+        interpreter = Interpreter(['308', '100'])
+        interpreter.execute()
+        self.assertEquals('008', interpreter.get_register_value(0))
+
+        interpreter = Interpreter(['308', '302', '100'])
+        interpreter.execute()
+        self.assertEquals('010', interpreter.get_register_value(0))
+
+    def test_4dn_should_multiply_d_by_n(self):
+        interpreter = Interpreter(['408', '100'])
+        interpreter.execute()
+        self.assertEquals('000', interpreter.get_register_value(0))
+
+        interpreter = Interpreter(['302', '407', '100'])
+        interpreter.execute()
+        self.assertEquals('014', interpreter.get_register_value(0))
+
+    def test_5ds_should_set_register_d_to_the_value_of_register_s(self):
+        interpreter = Interpreter(['209', '510', '100'])
+        interpreter.execute()
+        self.assertEquals('009', interpreter.get_register_value(0))
+        self.assertEquals('009', interpreter.get_register_value(1))
