3b6116fd57a01f7dce1d691fe15a6ec1a6d6bae7
linus123 <linus_pagan@hotmail.com>
2018-11-04 05:06:43 -0600

    Completed Jolly jumper

--- src/10038_JollyJumgers/jolly_jumper.py
+++ src/10038_JollyJumgers/jolly_jumper.py
@@ -1,13 +1,47 @@
 def is_jolly_jumper(values):
-    if len(values) <= 0:
+    values_len = len(values)
+
+    if values_len <= 0:
         raise ValueError("values must have at least one value")
 
-    if len(values) == 1:
+    if values_len == 1:
         return True
 
-    diff1 = abs(values[0]) - abs(values[1])
+    differences = {}
 
-    if abs(diff1) == 1:
+    for current_index in range(values_len - 1):
+        next_index = current_index + 1
+
+        current_difference = get_abs_diff(
+            values[current_index],
+            values[next_index])
+
+        differences[current_difference] = 1
+
+    if is_difference_values_len_minus_one(differences, values_len):
+        return False
+
+    if has_successive_elements(differences, values_len):
         return True
 
     return False
+
+
+def has_successive_elements(differences, values_len):
+    for count in range(values_len - 1):
+        if (count + 1) not in differences:
+            return False
+
+    return True
+
+
+def is_difference_values_len_minus_one(diff_dic, values_len):
+    return len(diff_dic) != (values_len - 1)
+
+
+def get_abs_diff(current_value, next_value):
+
+    curr_abs = abs(current_value)
+    next_abs = abs(next_value)
+
+    return abs(curr_abs - next_abs)
--- src/10038_JollyJumgers/jolly_jumper_tests.py
+++ src/10038_JollyJumgers/jolly_jumper_tests.py
@@ -13,8 +13,8 @@ class JollyJumperTests(unittest.TestCase):
 
     def test_should_return_true_when_given_array_with_single_item(self):
         from jolly_jumper import is_jolly_jumper
-        result = is_jolly_jumper([1])
 
+        result = is_jolly_jumper([1])
         self.assertTrue(result)
 
     def test_should_return_false_when_diff_of_two_items_is_not_one(self):
@@ -32,6 +32,24 @@ class JollyJumperTests(unittest.TestCase):
         result = is_jolly_jumper([-100, -122])
         self.assertFalse(result)
 
+    def test_should_return_false_when_diff_more_than_one_give_three_values(self):
+        from jolly_jumper import is_jolly_jumper
+
+        result = is_jolly_jumper([1, 2, 5])
+        self.assertFalse(result)
+
+        result = is_jolly_jumper([1, 2, -5])
+        self.assertFalse(result)
+
+        result = is_jolly_jumper([-1, -2, -5])
+        self.assertFalse(result)
+
+        result = is_jolly_jumper([4, 1, 3])
+        self.assertFalse(result)
+
+        result = is_jolly_jumper([7, -6, -9])
+        self.assertFalse(result)
+
     def test_should_return_true_when_two_values_are_give_and_diff_is_one(self):
         from jolly_jumper import is_jolly_jumper
 
@@ -45,6 +63,42 @@ class JollyJumperTests(unittest.TestCase):
         self.assertTrue(result)
 
         result = is_jolly_jumper([1, 0])
+        self.assertTrue(result)
+
+        result = is_jolly_jumper([1, 2])
+        self.assertTrue(result)
+
+        result = is_jolly_jumper([2, -3])
+        self.assertTrue(result)
+
+    def test_should_return_true_given_three_values(self):
+        from jolly_jumper import is_jolly_jumper
+
+        result = is_jolly_jumper([0, 1, 3])
+        self.assertTrue(result)
+
+        result = is_jolly_jumper([0, -1, 3])
+        self.assertTrue(result)
+
+        result = is_jolly_jumper([0, -1, -3])
+        self.assertTrue(result)
+
+        result = is_jolly_jumper([6, 7, 9])
+        self.assertTrue(result)
+
+        result = is_jolly_jumper([-6, -7, 9])
+        self.assertTrue(result)
+
+        result = is_jolly_jumper([-6, 7, -9])
+        self.assertTrue(result)
+
+        result = is_jolly_jumper([-5, -7, 8])
+        self.assertTrue(result)
+
+    def test_random_true_test(self):
+        from jolly_jumper import is_jolly_jumper
+
+        result = is_jolly_jumper([1, 4, 2, 3])
         self.assertTrue(result)
 
 
