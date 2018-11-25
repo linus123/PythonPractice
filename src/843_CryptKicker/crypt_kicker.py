e49b46e423886b6f0d95b5aa96d4edbb315a2d7c
Paul Herrera <linus_pagan@hotmail.com>
2018-11-25 09:14:11 -0600

    Made progress on crypt

--- src/843_CryptKicker/crypt_kicker.py
+++ src/843_CryptKicker/crypt_kicker.py
@@ -1,44 +1,50 @@
-def crypt_decrypt(encrypted_line, word_dictionary):
+def crypt_decrypt(encrypted_line, solution_words):
 
-    encrypted_line = encrypted_line.strip()
-    encrypted_words = encrypted_line.split(' ')
-    encrypted_words = [word.strip() for word in encrypted_words]
+    encrypted_words = convert_to_array(encrypted_line)
 
-    word_dictionary = [word.strip() for word in word_dictionary]
-
-    if encrypted_line == "":
+    if len(encrypted_words) == 0:
         return ""
 
-    decode_letters = {}
+    solution_words = clean_array(solution_words)
+
+    word_map = WordMap(solution_words)
 
     for encrypted_word in encrypted_words:
-        if encrypted_word in decode_letters:
-            f = decode_letters[encrypted_word]
-            for dic_work in word_dictionary:
-                if len(f) == 0:
-                    return get_no_solution(encrypted_words)
-        else:
-            f = []
+        word_map.process_encrypted_word(encrypted_word)
 
-            if is_already_mapped(word_dictionary[0], decode_letters):
-                return get_no_solution(encrypted_words)
+    has_solution = word_map.has_solution()
 
-            f.append(word_dictionary[0])
-            decode_letters[encrypted_word] = f
+    if not has_solution:
+        return get_no_solution(encrypted_words)
 
     s = ""
 
     for encrypted_word in encrypted_words:
-        s += str(decode_letters[encrypted_word][0]) + " "
+        s += str(word_map.get_word(encrypted_word)) + " "
 
     return s.strip()
+
+
+def clean_array(word_dictionary):
+    return [word.strip() for word in word_dictionary]
+
+
+def convert_to_array(encrypted_line):
+    encrypted_line = encrypted_line.strip()
+
+    if encrypted_line == "":
+        return []
+
+    encrypted_words = encrypted_line.split(' ')
+    encrypted_words = [word.strip() for word in encrypted_words]
+    return encrypted_words
 
 
 def get_no_solution(encrypted_words):
     s = ""
 
     for _ in encrypted_words:
-        s += "* "
+        s += ('*' * len(_)) + " "
 
     return s.strip()
 
@@ -48,3 +54,55 @@ def is_already_mapped(letter, decode_letters):
         if letter in letter_array:
             return True
     return False
+
+
+class WordMap:
+    def __init__(self, solution_words):
+        self.solution_words = solution_words
+        self.decode_words = {}
+
+    def process_encrypted_word(self, encrypted_word):
+        single_letter_options = []
+
+        for solution_word in self.solution_words:
+            single_letter_options.append(solution_word)
+            self.decode_words[encrypted_word] = single_letter_options
+
+    def has_solution(self):
+        for key, letter_array in self.decode_words.items():
+            if len(letter_array) == 1:
+                still_as_solution = self.remove_letter_from_options(letter_array[0], self.decode_words, key)
+                if not still_as_solution:
+                    return False
+
+        # for multiples .. pick one
+        for key, letter_array in self.decode_words.items():
+            if len(letter_array) > 1:
+                # delete all items except first one
+                del letter_array[1:]
+                still_as_solution = self.remove_letter_from_options(letter_array[0], self.decode_words, key)
+                if not still_as_solution:
+                    return False
+
+        return True
+
+    def remove_letter_from_options(self, letter_to_remove, decode_letters, key_to_skip):
+        for key, letter_array in decode_letters.items():
+            if key != key_to_skip:
+                letter_array.remove(letter_to_remove)
+                if len(letter_array) == 0:
+                    return False
+        return True
+
+    def get_word(self, encrypted_word):
+        return self.decode_words[encrypted_word][0]
+
+
+class LetterMap:
+    def __init__(self, solution_words):
+        self.solution_words = solution_words
+        self.decode_letters = {}
+
+    def process_encrypted_word(self, word):
+        pass
+
--- src/843_CryptKicker/crypt_kicker_tests.py
+++ src/843_CryptKicker/crypt_kicker_tests.py
@@ -44,6 +44,21 @@ class CryptKickerTests(unittest.TestCase):
         result = crypt_decrypt("a", dictionary)
         self.assertEqual("c", result)
 
+    def test_should_return_solution_given_two_single_letter_dictionary_and_two_single_letter_encrypt(self):
+        dictionary = ["a", "b"]
+
+        result = crypt_decrypt("c d", dictionary)
+        self.assertEqual("a b", result)
+
+        result = crypt_decrypt("d c", dictionary)
+        self.assertEqual("a b", result)
+
+        result = crypt_decrypt("c d d", dictionary)
+        self.assertEqual("a b b", result)
+
+        result = crypt_decrypt("c c d", dictionary)
+        self.assertEqual("a a b", result)
+
     def test_should_return_solution_give_two_single_letter_words_without_single_solution(self):
         dictionary = ["a"]
         result = crypt_decrypt("b c", dictionary)
@@ -53,10 +68,67 @@ class CryptKickerTests(unittest.TestCase):
         result = crypt_decrypt("b c", dictionary)
         self.assertEqual("* *", result)
 
-    def test_should_return_solution_give_two_single_letter_words(self):
+    def test_should_return_solution_given_two_single_letter_words(self):
         dictionary = ["a"]
         result = crypt_decrypt("c c", dictionary)
         self.assertEqual("a a", result)
+
+    def test_should_return_solution_given_three_single_letter_words(self):
+        dictionary = ["a"]
+        result = crypt_decrypt("c c c", dictionary)
+        self.assertEqual("a a a", result)
+
+    def test_should_return_no_solution_when_two_letter_words_have_no_solution(self):
+        dictionary = ["aa"]
+        result = crypt_decrypt("bb cc", dictionary)
+        self.assertEqual("** **", result)
+
+    def test_should_return_no_solution_with_three_letters(self):
+        dictionary = ["aab", "aac"]
+        result = crypt_decrypt("xxy xxz", dictionary)
+        self.assertEqual("aab acc", result)
+
+    def test_should_return_no_solution_when_two_letter_words_have_no_solution_and_change_in_letters(self):
+        dictionary = ["ab"]
+        result = crypt_decrypt("bc cd", dictionary)
+        self.assertEqual("** **", result)
+
+        dictionary = ["ab"]
+        result = crypt_decrypt("bc bd", dictionary)
+        self.assertEqual("** **", result)
+
+        dictionary = ["ab"]
+        result = crypt_decrypt("cc dd", dictionary)
+        self.assertEqual("** **", result)
+
+    def test_should_return_solution_when_two_letter_words_have_single_solution(self):
+        dictionary = ["aa"]
+        result = crypt_decrypt("bb bb", dictionary)
+        self.assertEqual("aa aa", result)
+
+    def test_should_return_solution_when_two_letter_words_have_single_solution_with_different_letters(self):
+        dictionary = ["ab"]
+        result = crypt_decrypt("bc bc", dictionary)
+        self.assertEqual("ab ab", result)
+
+    def test_should_return_solution_one_of_multiple_solutions_with_two_letters(self):
+        dictionary = ["ab", "cd"]
+        result = crypt_decrypt("xy xy", dictionary)
+        self.assertEqual("ab ab", result)
+
+        dictionary = ["cd", "ab"]
+        result = crypt_decrypt("xy xy", dictionary)
+        self.assertEqual("cd cd", result)
+
+    def test_should_return_solution_one_of_multiple_solutions_with_two_letters_when_first_item_should_be_rejected(self):
+        dictionary = ["xx", "cd"]
+        result = crypt_decrypt("xy", dictionary)
+        self.assertEqual("cd cd", result)
+
+    def test_would_work_with_singe_word_and_single_encrypt_word(self):
+        dictionary = ["abc"]
+        result = crypt_decrypt("xyx", dictionary)
+        self.assertEqual("abc", result)
 
 
 def main():
