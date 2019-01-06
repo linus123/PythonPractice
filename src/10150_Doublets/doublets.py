b64d8136cda5f99c112e37f3e30d4c34505ad496
Paul Herrera <linus_pagan@hotmail.com>
2019-01-06 06:25:39 -0600

    letter difference done

--- src/10150_Doublets/doublets.py
+++ src/10150_Doublets/doublets.py
@@ -4,11 +4,51 @@ def has_more_than_one_difference(word1: str, word2: str):
 
     diff_count = 0
 
-    for letter_index in range(len(word1)):
-        if word1[letter_index] != word2[letter_index]:
+    longer_len = len(word1)
+
+    if len(word2) > longer_len:
+        longer_len = len(word2)
+
+    letter_index = 0
+    word1_index = 0
+    word2_index = 0
+
+    while letter_index < longer_len:
+
+        if word1_index >= len(word1):
             diff_count += 1
             if diff_count > 1:
                 return True
+
+        elif word2_index >= len(word2):
+            diff_count += 1
+            if diff_count > 1:
+                return True
+
+        elif word1[word1_index] != word2[word2_index]:
+            diff_count += 1
+            if diff_count > 1:
+                return True
+
+            if word1_index + 1 < len(word1)\
+                    and word1[word1_index + 1] == word2[word2_index]:
+
+                word2_index -= 1
+
+                if len(word1) == len(word2):
+                    longer_len += 1
+
+            if word2_index + 1 < len(word2)\
+                    and word1[word1_index] == word2[word2_index + 1]:
+
+                word1_index -= 1
+
+                if len(word1) == len(word2):
+                    longer_len += 1
+
+        word1_index += 1
+        word2_index += 1
+        letter_index += 1
 
     return False
 
--- src/10150_Doublets/doublets_tests.py
+++ src/10150_Doublets/doublets_tests.py
@@ -43,3 +43,51 @@ class DoubletsTests(unittest.TestCase):
 
         result = has_more_than_one_difference("sum", "fom")
         self.assertTrue(result)
+
+    def test_204(self):
+        """has_more_than_one_difference should return false if only one letter is missing at the end"""
+
+        result = has_more_than_one_difference("sum", "sumo")
+        self.assertFalse(result)
+
+        result = has_more_than_one_difference("sumo", "sum")
+        self.assertFalse(result)
+
+    def test_205(self):
+        """has_more_than_one_difference should return false when only a single letter is missing from a word"""
+
+        result = has_more_than_one_difference("foobar", "fobar")
+        self.assertFalse(result)
+
+        result = has_more_than_one_difference("fobar", "foobar")
+        self.assertFalse(result)
+
+        result = has_more_than_one_difference("fo", "f")
+        self.assertFalse(result)
+
+        result = has_more_than_one_difference("f", "fo")
+        self.assertFalse(result)
+
+    def test_206(self):
+        """foobar should return true when one letter is different and one letter is missing"""
+
+        result = has_more_than_one_difference("foobar", "fobara")
+        self.assertTrue(result)
+
+        result = has_more_than_one_difference("fobara", "foobar")
+        self.assertTrue(result)
+
+        result = has_more_than_one_difference("xoobar", "foobara")
+        self.assertTrue(result)
+
+        result = has_more_than_one_difference("foobara", "xoobar")
+        self.assertTrue(result)
+
+    def test_207(self):
+        """Should return true when missing two letters"""
+
+        result = has_more_than_one_difference("foobar", "foobaroo")
+        self.assertTrue(result)
+
+        result = has_more_than_one_difference("foobaroo", "foobar")
+        self.assertTrue(result)
