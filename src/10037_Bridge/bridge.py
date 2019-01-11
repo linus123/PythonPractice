8ad26e34cb37788600d7f7bcc254c86c62a24d1d
Paul Herrera <linus_pagan@hotmail.com>
2019-01-11 10:38:30 -0600

    added next test case

--- src/10037_Bridge/bridge.py
+++ src/10037_Bridge/bridge.py
@@ -1,2 +1,8 @@
 def get_min_time_to_cross(people: list):
-    return people[0]
+    if len(people) == 1:
+        return people[0]
+
+    if people[0] > people[1]:
+        return people[0]
+
+    return people[1]
--- src/10037_Bridge/bridge_tests.py
+++ src/10037_Bridge/bridge_tests.py
@@ -8,7 +8,16 @@ class BridgeTests(unittest.TestCase):
         """ShouldR return the value of the of the given time given a single person"""
 
         people = [1]
-
         result = get_min_time_to_cross(people)
-
         self.assertEqual(1, result)
+
+    def test_002(self):
+        """Should return the time of the large person give two people"""
+
+        people = [1, 2]
+        result = get_min_time_to_cross(people)
+        self.assertEqual(2, result)
+
+        people = [1, 10]
+        result = get_min_time_to_cross(people)
+        self.assertEqual(10, result)
