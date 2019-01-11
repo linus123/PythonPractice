6802a85102ed538a184e0f6059fdeda4b2c8b765
Paul Herrera <linus_pagan@hotmail.com>
2019-01-11 10:50:52 -0600

    added result object

--- src/10037_Bridge/bridge.py
+++ src/10037_Bridge/bridge.py
@@ -1,8 +1,26 @@
-def get_min_time_to_cross(people: list):
+class CrossResult:
+    def __init__(self) -> None:
+        self.total_number_of_seconds = 0
+        self.crossings = []
+
+    def add_crossing(self, crossing: list):
+        self.crossings.append(crossing)
+
+
+def get_min_time_to_cross(people: list) -> CrossResult:
     if len(people) == 1:
-        return people[0]
+        cr = CrossResult()
+        cr.total_number_of_seconds = people[0]
+        cr.add_crossing([people[0]])
+        return cr
 
     if people[0] > people[1]:
-        return people[0]
+        cr = CrossResult()
+        cr.total_number_of_seconds = people[0]
+        cr.add_crossing([people[0], people[1]])
+        return cr
 
-    return people[1]
+    cr = CrossResult()
+    cr.total_number_of_seconds = people[1]
+    cr.add_crossing([people[0], people[1]])
+    return cr
--- src/10037_Bridge/bridge_tests.py
+++ src/10037_Bridge/bridge_tests.py
@@ -9,15 +9,28 @@ class BridgeTests(unittest.TestCase):
 
         people = [1]
         result = get_min_time_to_cross(people)
-        self.assertEqual(1, result)
+        self.assertEqual(1, result.total_number_of_seconds)
+        self.assertEqual(1, len(result.crossings))
+        self.assertEqual([1], result.crossings[0])
 
     def test_002(self):
         """Should return the time of the large person give two people"""
 
         people = [1, 2]
         result = get_min_time_to_cross(people)
-        self.assertEqual(2, result)
+        self.assertEqual(2, result.total_number_of_seconds)
+        self.assertEqual(1, len(result.crossings))
+        self.assertEqual([1, 2], result.crossings[0])
 
         people = [1, 10]
         result = get_min_time_to_cross(people)
-        self.assertEqual(10, result)
+        self.assertEqual(1, len(result.crossings))
+        self.assertEqual(10, result.total_number_of_seconds)
+        self.assertEqual([1, 10], result.crossings[0])
+
+    # def test_003(self):
+    #     """Should return expected values given sample test problem"""
+    #
+    #     people = [1, 2]
+    #     result = get_min_time_to_cross(people)
+    #     self.assertEqual(2, result)
